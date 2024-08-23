1b583cdc466ae42b7b27ff9a3dbf907304b045fa
Sharan Kumar <yenugulasharan8019@gmail.com>
2024-08-23 21:22:24 +0530

    added reset styles

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1>YanJav</h1>
+      <p>Flavor Meets Convenience: Your Daily Cuisine & Grocery Essentials!</p>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  );
+}
+
+export default App;
